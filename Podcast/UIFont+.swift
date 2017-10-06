c8151c2378732a3e653d667838f5c566cca12e81
natashaarmbrust <natasha.armbrust@gmail.com>
2017-10-06 19:46:32 -0400

    adding styleguide

--- Podcast/UIFont+.swift
+++ Podcast/UIFont+.swift
@@ -3,6 +3,74 @@ import UIKit
 
 extension UIFont {
     
+    class func _40SemiboldBlackFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 20.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _32SemiboldBlackFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 16.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _32SemiboldGrayFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 16.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _32RegularWhiteFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 16.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _28SemiboldGrayFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _28SemiboldBlackFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _32SemiboldWhiteFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _28SemiboldWhiteFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _28RegularBlackFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.regular)
+    }
+    
+    class func _28RegularGrayFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.regular)
+    }
+    
+    class func _28RegularWhiteFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.regular)
+    }
+    
+    class func _28RegularTealFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 14.0, weight: UIFont.Weight.regular)
+    }
+    
+    class func _24SemiboldBlackFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _24SemiboldTealFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.semibold)
+    }
+    
+    class func _24RegularGrayFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.regular)
+    }
+    
+    class func _24RegularBlackFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.regular)
+    }
+    
+    class func _24RegularTealFont() -> UIFont {
+        return UIFont.systemFont(ofSize: 12.0, weight: UIFont.Weight.regular)
+    }
+    
     @nonobjc static let discoverTableViewCellDefaultFontAttributes = [NSAttributedStringKey.foregroundColor: UIColor.podcastGrayLight, NSAttributedStringKey.font: UIFont(name: ".SFUIText-Medium", size: 10.0)!]
     
      @nonobjc static let navigationBarDefaultFontAttributes = [NSAttributedStringKey.foregroundColor: UIColor.podcastBlack, NSAttributedStringKey.font: UIFont(name: ".SFUIText-Medium", size: 16.0)!]
