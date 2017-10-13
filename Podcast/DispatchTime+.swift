9d083f056a2d96ea1480ced786ec14952d61d47c
Mindy Lou <mml234@cornell.edu>
2017-10-12 22:40:15 -0400

    address pr comments part 2

--- Podcast/DispatchTime+.swift
+++ Podcast/DispatchTime+.swift
@@ -7,7 +7,7 @@
 //
 
 extension DispatchTime {
-    static func waitFor(milliseconds: Int, completion: @escaping () -> Void) {
+    static func waitFor(milliseconds: Int, completion: @escaping () -> ()) {
         let deadlineTime = DispatchTime.now() + .milliseconds(milliseconds)
         DispatchQueue.main.asyncAfter(deadline: deadlineTime) {
             completion()
