5e9f23558cdebbac48236a75facf6c37f3d5fdc1
natashaarmbrust <natasha.armbrust@gmail.com>
2017-11-14 18:14:35 -0500

    cooler loading animator

--- Podcast/ActivitityIndicatorView.swift
+++ Podcast/ActivitityIndicatorView.swift
@@ -15,8 +15,8 @@ class LoadingAnimatorUtilities {
 
     static func createLoadingAnimator() -> NVActivityIndicatorView {
         let color: UIColor = .sea
-        let type: NVActivityIndicatorType = .ballTrianglePath
-        let frame = CGRect(x: 0, y: 0, width: loadingAnimatorSize, height: loadingAnimatorSize)
+        let type: NVActivityIndicatorType = .lineScalePulseOutRapid
+        let frame = CGRect(x: 0, y: 0, width: loadingAnimatorSize, height: 2/3 * loadingAnimatorSize)
         return NVActivityIndicatorView(frame: frame, type: type, color: color, padding: 0)
     }
 
