e89398cd138de61828029f39cc3cf0009dedebec
Daniel Li <dl743@cornell.edu>
2017-11-15 20:02:58 -0500

    Remove extraneous lines

--- Podcast/ImageView.swift
+++ Podcast/ImageView.swift
@@ -1,11 +1,8 @@
 import Kingfisher
-import NVActivityIndicatorView
 
 class ImageView: UIImageView {
     
     func setImageAsynchronouslyWithDefaultImage(url: URL?, defaultImage: UIImage = #imageLiteral(resourceName: "nullSeries")) {
-        guard frame != .zero else { return }
-
         kf.setImage(with: url, placeholder: defaultImage)
     }
 }
