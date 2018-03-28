9984afff969b24d5b91f443a9b6730cfae9a13a7
natashaarmbrust <natasha.armbrust@gmail.com>
2018-03-28 11:21:54 -0400

    continuing to fix

--- Podcast/ViewController.swift
+++ Podcast/ViewController.swift
@@ -40,10 +40,12 @@ class ViewController: UIViewController {
     
     func stylizeNavBar() {
         navigationController?.navigationBar.tintColor = .sea
-        navigationController?.navigationBar.setBackgroundImage(UIColor.offWhite.as1ptImage(), for: .default)
+        //navigationController?.navigationBar.setBackgroundImage(UIColor.offWhite.as1ptImage(), for: .default)
         navigationController?.navigationBar.backgroundColor = .offWhite
         navigationController?.navigationBar.barTintColor = .offWhite
         navigationController?.navigationBar.shadowImage = UIColor.silver.as1ptImage()
+        guard let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView else { return }
+        statusBar.backgroundColor = .offWhite
     }
     
     var mainScrollView: UIScrollView?
