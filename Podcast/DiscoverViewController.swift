3407634eab510ea8c64832d9629daa354b1535f2
Jack Wilson Thompson <jwt84@cornell.edu>
2018-05-01 21:34:59 -0400

    removing markers

--- Podcast/DiscoverViewController.swift
+++ Podcast/DiscoverViewController.swift
@@ -172,17 +172,11 @@ class DiscoverViewController: DiscoverComponentViewController {
         fetchEpisodes()
 
     }
-<<<<<<< HEAD
-
-    func fetchEpisodes() {
-        
-=======
     func fetchEpisodes(canPullToRefresh: Bool = false) {
         if canPullToRefresh {
             offset = 0
         }
-      
->>>>>>> 65b6881c330ebe6a261feb158afaff98149dd477
+
         let getEpisodesEndpointRequest = DiscoverUserEndpointRequest(requestType: .episodes, offset: offset, max: pageSize)
         getEpisodesEndpointRequest.success = { response in
             guard let episodes = response.processedResponseValue as? [Episode] else { return }
@@ -193,12 +187,8 @@ class DiscoverViewController: DiscoverComponentViewController {
             self.offset += self.pageSize
             self.topEpisodesTableView.finishInfiniteScroll()
             self.topEpisodesTableView.reloadData()
-<<<<<<< HEAD
-            self.episodesLoadingAnimation.stopAnimating()
-=======
             self.topEpisodesTableView.refreshControl?.endRefreshing()
             self.loadingAnimation.stopAnimating()
->>>>>>> 65b6881c330ebe6a261feb158afaff98149dd477
         }
 
         getEpisodesEndpointRequest.failure = { _ in
