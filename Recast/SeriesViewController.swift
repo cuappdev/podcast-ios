a9bcf9cb5f325cf4341be818020c2e7ae809340a
Drew Dunne <drewsdunne1@gmail.com>
2018-09-12 20:23:41 -0400

    started new project for redesign

---
+++ Recast/SeriesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recast
+//
+//  Created by Drew Dunne on 9/12/18.
+//  Copyright © 2018 Cornell AppDev. All rights reserved.
+//
+
+import UIKit
+
+class SeriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .blue
+    }
+
+}
+
