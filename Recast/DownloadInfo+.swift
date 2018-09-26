7efe551d64e2c8d5fd75a2f083cd7bbd9240374a
Mindy Lou <mml234@cornell.edu>
2018-09-26 12:24:49 -0400

    Continue with download manager by storing information in Core Data

---
+++ Recast/DownloadInfo+.swift
@@ -0,0 +1,63 @@
+//
+//  DownloadInfo+.swift
+//  Recast
+//
+//  Created by Mindy Lou on 9/22/18.
+//  Copyright © 2018 Cornell AppDev. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+// MARK: - DownloadInfo Entity Name Keys
+extension DownloadInfo {
+    struct Keys {
+        static let entityName = "DownloadInfo"
+        static let downloadedAt = "downloadedAt"
+        static let identifier = "identifier"
+        static let path = "path"
+        static let progress = "progress"
+        static let resumeData = "resumeData"
+        static let sizeInBytes = "sizeInBytes"
+        static let status = "status"
+        static let episode = "episode"
+    }
+}
+
+extension EpisodeMO {
+    struct Keys {
+        static let entityName = "EpisodeMO"
+        static let downloadInfo = "downloadInfo"
+    }
+}
+
+// MARK: - DownloadInfo + Core Data Properties
+extension DownloadInfo {
+    class func fetchRequestForIdentifier(_ identifier: Int) -> NSFetchRequest<DownloadInfo> {
+        let fetchRequest = NSFetchRequest<DownloadInfo>(entityName: DownloadInfo.Keys.entityName)
+        fetchRequest.predicate = NSPredicate(format: "\(DownloadInfo.Keys.identifier) = %@", identifier)
+        return fetchRequest
+    }
+
+    class func fetchDownloadInfo(with identifier: Int) -> DownloadInfo? {
+        let fetchRequest = DownloadInfo.fetchRequestForIdentifier(identifier)
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return nil }
+        do {
+            let results = try appDelegate.dataController.managedObjectContext.fetch(fetchRequest)
+            if let downloadInfo = results.first, results.count == 1 {
+                return downloadInfo
+            }
+        } catch {
+            print("Error fetching data from context")
+        }
+        return nil
+    }
+}
+
+// MARK: - DownloadInfo Status
+struct DownloadInfoStatus {
+    static let failed = "failed"
+    static let canceled = "canceled"
+    static let succeeded = "succeeded"
+}
