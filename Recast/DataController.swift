98b583289d1881bf1f9e375358904bc40bf0ce8d
Mindy Lou <mml234@cornell.edu>
2018-09-28 11:17:00 -0400

    Add mark for DataController

--- Recast/DataController.swift
+++ Recast/DataController.swift
@@ -10,6 +10,8 @@ import Foundation
 import CoreData
 
 class DataController: NSObject {
+
+    // MARK: Data variables
     var managedObjectContext: NSManagedObjectContext
     var persistentContainer: NSPersistentContainer
 
