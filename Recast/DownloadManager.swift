251e7a55e69aa4542b4d1c61b7db4a5312cc3c3f
Mindy Lou <mml234@cornell.edu>
2018-09-29 14:34:29 -0400

    Add url info to notifications

--- Recast/DownloadManager.swift
+++ Recast/DownloadManager.swift
@@ -39,8 +39,8 @@ class DownloadManager: NSObject {
         return URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue())
     }
 
-    /// Maps download url to the task handling the download
-    private var downloadedUrls: [URL: URLSessionDownloadTask] = [:]
+    /// Maps task handling the download to the download task
+    private var downloadedUrls: [URLSessionDownloadTask: URL] = [:]
 
     func download(episode: EpisodeMO) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate,
@@ -49,7 +49,7 @@ class DownloadManager: NSObject {
             let url = episode.audioURL else { return }
 
         let task = session.downloadTask(with: url)
-        downloadedUrls[url] = task
+        downloadedUrls[task] = url
         let downloadInfo = NSManagedObject(entity: entity, insertInto: appDelegate.dataController.managedObjectContext)
         downloadInfo.setValuesForKeys([
             DownloadInfo.Keys.progress: 0,
@@ -62,7 +62,9 @@ class DownloadManager: NSObject {
     }
 
     func cancel(episode: EpisodeMO) {
-        guard let url = episode.audioURL, let task = downloadedUrls[url] else { return }
+        guard let url = episode.audioURL else { return }
+        let taskForURL = downloadedUrls.filter{ $0.value == url }.map{ $0.key }.first
+        guard let task = taskForURL else { return }
         task.cancel(byProducingResumeData: { resumeData in
             // store resumeData in DownloadInfo
             if let data = resumeData,
@@ -82,7 +84,7 @@ class DownloadManager: NSObject {
         guard let url = episode.audioURL, let downloadInfo = episode.downloadInfo,
             let resumeData = downloadInfo.resumeData else { return }
         let task = session.downloadTask(withResumeData: resumeData)
-        downloadedUrls[url] = task
+        downloadedUrls[task] = url
         downloadInfo.setValue(task.taskIdentifier, forKey: DownloadInfo.Keys.identifier)
         saveData()
         task.resume()
@@ -105,7 +107,8 @@ extension DownloadManager: URLSessionDownloadDelegate {
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate,
             let entity = NSEntityDescription.entity(forEntityName: DownloadInfo.Keys.entityName,
-                                                    in: appDelegate.dataController.managedObjectContext)
+                                                    in: appDelegate.dataController.managedObjectContext),
+            let downloadURL = downloadedUrls[downloadTask]
             else { return }
         let downloadInfo = NSManagedObject(entity: entity, insertInto: appDelegate.dataController.managedObjectContext)
         downloadInfo.setValuesForKeys([
@@ -117,16 +120,17 @@ extension DownloadManager: URLSessionDownloadDelegate {
         saveData()
         // Send notification that download is complete
         // Do we need to send user info?
-        NotificationCenter.default.post(name: .didCompleteDownload, object: nil)
+        let userInfoDict = ["url": downloadURL]
+        NotificationCenter.default.post(name: .didCompleteDownload, object: nil, userInfo: userInfoDict)
     }
 
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64,
                     totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        if totalBytesExpectedToWrite > 0 {
+        if let downloadURL = downloadedUrls[downloadTask], totalBytesExpectedToWrite > 0 {
             let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
-            let progressDict = ["progress": progress]
+            let userInfoDict: [String: Any] = ["progress": progress, "url": downloadURL]
             // Send download progress notification
-            NotificationCenter.default.post(name: .didUpdateDownloadProgress, object: nil, userInfo: progressDict)
+            NotificationCenter.default.post(name: .didUpdateDownloadProgress, object: nil, userInfo: userInfoDict)
         }
     }
 
@@ -134,11 +138,13 @@ extension DownloadManager: URLSessionDownloadDelegate {
         // Fetch download info by identifier
         if let downloadInfo = DownloadInfo.fetchDownloadInfo(with: task.taskIdentifier) {
             downloadInfo.setValue(DownloadInfoStatus.failed, forKey: DownloadInfo.Keys.status)
+            saveData()
         }
-        saveData()
         // Send notification about error
-        // Do we need to send user info?
-        NotificationCenter.default.post(name: .didFailDownload, object: nil)
+        guard let downloadTask = task as? URLSessionDownloadTask,
+            let downloadURL = downloadedUrls[downloadTask] else { return }
+        let userInfoDict = ["url": downloadURL]
+        NotificationCenter.default.post(name: .didFailDownload, object: nil, userInfo: userInfoDict)
     }
 
 }
